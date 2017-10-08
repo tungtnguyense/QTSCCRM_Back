7ac75611f912bceb779590a40edeb8cd1648444c
tungtnguyense <tungtnguyense@gmail.com>
2017-10-09 02:51:25 +0700

    update result entity

--- APIProject.Model/Models/MarketingResult.cs
+++ APIProject.Model/Models/MarketingResult.cs
@@ -17,6 +17,27 @@ namespace APIProject.Model.Models
 
         public int? ContactId { get; set; }
 
+        public string CustomerName { get; set; }
+        public string ContactFirstName { get; set; }
+        public string ContactMiddleName { get; set; }
+        public string ContactLastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Notes { get; set; }
+        public int FacilityRate { get; set; }
+        public int ArrangingRate { get; set; }
+        public int ServicingRate { get; set; }
+        public int IndicatorRate { get; set; }
+        public int OthersRate { get; set; }
+        public bool Media { get; set; }
+        public bool InvitationLetter { get; set; }
+        public bool QTSCWebsite { get; set; }
+        public bool Friend { get; set; }
+        public bool FromOthers { get; set; }
+        public bool WantAnother { get; set; }
+
+
         public virtual Contact Contact { get; set; }
 
         public virtual Customer Customer { get; set; }
